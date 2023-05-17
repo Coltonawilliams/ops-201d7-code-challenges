03c54c850b75edd2005bd945b8faf511549f849c
Coltonawilliams <williamscaw3@gmail.com>
2023-05-17 19:35:22 -0400

    Domain Analysis

---
+++ whois.sh
@@ -0,0 +1,14 @@
+# Script: Ops 201 Class 12 Ops Challenge Solution
+# Author: Colton Williams
+# Date of latest revision: 05/17/23
+# Purpose: Domain Analysis
+
+echo "enter website"
+read website
+
+# (>>) inserts on the bottom line
+whois $website > domainanalysis.txt 
+dig $website >> domainanalysis.txt 
+host $website >> domainanalysis.txt 
+nslookup $website >> domainanalysis.txt 
+
