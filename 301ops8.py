1f31a1c7e3fd95797c30654fe591a7d3ad0cfa8d
Coltonawilliams <williamscaw3@gmail.com>
2023-06-26 20:46:14 -0400

    Variables

---
+++ 301ops8.py
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Script: Ops 301 Class 08 Ops Challenege Solution
+#Author: Colton Williams
+#Date of last revision: 26 June 2023
+#Purpose: Variables
+
+Starbattle = ["Steam", "I need a job", "art of darts", "Python makes me sad", "Savvy what?", "Hot Mess", "Dohicky", "Where are my socks", "Destination lucked", "Sweet"] 
+print(Starbattle[3])
+print(Starbattle[5:])
+Starbattle[6] = "Onion"
+print(Starbattle)
